382cbf2142f404dc9bb14688a2b3f44d84b8ff19
aleforion <aleforion@orion.com>
2017-12-19 23:58:57 -0300

    Filtro categorias concluido

--- core/views/conta.py
+++ core/views/conta.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, redirect
 from core.models.controle import Conta, Categoria
 from core.forms.formulario import ContaForm
 from django.db.models import Sum
+from django.http import JsonResponse
 
 
 def inicio (request):
-    soma = 0
     dados = {}
-    dados['form'] = ContaForm()
     dados['lista_conta'] = Conta.objects.all().order_by('-descricao')
     dados['soma'] = Conta.objects.all().aggregate(Sum('valor'))
+    dados['vPagos'] = Conta.objects.filter(pago=True).aggregate(Sum('valor'))
+    dados['vNaoPagos'] = Conta.objects.filter(pago=False).aggregate(Sum('valor'))
     dados['categorias'] = Categoria.objects.all()
     return render(request, 'core/lista_conta.html', dados)
 
@@ -17,11 +18,21 @@ def inicio (request):
 def inserir_conta(request):
     dados = {}
     dados['form'] = ContaForm()
-    dados['lista_conta'] = Conta.objects.all()
     conta = ContaForm(request.POST)
     if conta.is_valid():
         conta.save()
-        return redirect('inicio')
+        lista_conta = {}
+        dados_conta = []
+        contas = Conta.objects.all().order_by('descricao')
+        for c in contas:
+            if c.vencimento == "" or c.vencimento == None:
+                c.vencimento = ""
+            else:
+                registro_conta = {'descricao': c.descricao, 'categoria': c.categoria.nome, 'valor': c.valor,
+                                  'vencimento': c.vencimento, 'pago': c.pago, 'id': c.id}
+                dados_conta.append(registro_conta)
+            lista_conta['lista_conta'] = dados_conta
+        return JsonResponse(lista_conta)
     return render(request, 'core/inserir_conta.html', dados)
 
 
